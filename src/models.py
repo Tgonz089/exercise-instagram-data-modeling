278992522909413756c363cf21ed74e06f272686
Tgonz089 <tristangonzales8936@gmail.com>
2022-10-23 15:04:43 +0000

    Copied code from my other repo since there is an issue with working in the forked repo.

--- src/models.py
+++ src/models.py
@@ -8,23 +8,56 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(256), nullable = False)
+    firstname = Column(String(256))
+    lastname = Column(String(256))
+    email = Column(String(256), nullable = False)
+    password = Column (String(256), nullable = False)
+
+class Follower(Base):
+    __tablename__ = 'Follower'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    User_id = Column(Integer, ForeignKey('User.id'))
+    User = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'Post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    date = Column(String(256))
+    likescounter = Column(Integer)
+    User_id = Column(Integer, ForeignKey('User.id'))
+    User = relationship(User)
+
+class Media(Base):
+    __tablename__ = 'Media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    url = Column(String(256))
+    type = Column(String(256))
+    Post_id = Column(Integer, ForeignKey('Post.id'))
+    Post = relationship(Post)
+    
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(256))
+    Post_id = Column(Integer, ForeignKey('Post.id'))
+    Post = relationship(Post)
+    User_id = Column(Integer, ForeignKey('User.id'))
+    User = relationship(User)
 
     def to_dict(self):
         return {}
